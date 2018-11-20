5a5b1b2486c7cf4032ca8e179d4f387614d86535
hungnngreenglobal.vn <hungnn@greenglobal.vn>
2018-11-20 08:17:22 +0700

    fgjhgfhjhgjgh

---
+++ src/vn/toancauxanh/cms/service/CauHoiService.java
@@ -0,0 +1,14 @@
+package vn.toancauxanh.cms.service;
+
+import vn.toancauxanh.gg.model.CauHoi;
+import vn.toancauxanh.service.BasicService;
+
+public class CauHoiService extends BasicService<CauHoi>{
+	
+	private CauHoi cauHoi;
+
+	public CauHoi getCauHoi() {
+		return cauHoi;
+		
+	}	
+}
